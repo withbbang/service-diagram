f7e720fadd07d408b5b2e33c1af687029e73a403
unknown <withbbang@gmail.com>
2023-05-24 16:45:26 +0900

    라벨 수정 기능 추가

--- src/components/flow/Flow.tsx
+++ src/components/flow/Flow.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ReactFlow, {
   Background,
   Connection,
   Controls,
   Edge,
   MiniMap,
+  Node,
   addEdge,
   useEdgesState,
   useNodesState
@@ -13,19 +14,76 @@ import styles from './Flow.module.scss';
 import 'reactflow/dist/style.css';
 
 const Flow = () => {
+  const [id, setId] = useState<string>('-1');
+  const [nodeName, setNodeName] = useState<string>('');
+  const [edgeName, setEdgeName] = useState<string>('');
+
+  const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
+
   const initialNodes = [
     { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } }
   ];
-  const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+  const initialEdges = [
+    { id: 'e1-2', source: '1', target: '2', label: 'to the', type: 'step' }
+  ];
 
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === id) {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          node.data = {
+            ...node.data,
+            label: nodeName
+          };
+        }
+
+        return node;
+      })
+    );
+  }, [nodeName, setNodes]);
+
+  useEffect(() => {
+    setEdges((edges) =>
+      edges.map((edge) => {
+        if (edge.id === id) {
+          // it's important that you create a new object here
+          // in order to notify react flow about the change
+          edge = {
+            ...edge,
+            label: edgeName
+          };
+        }
+
+        return edge;
+      })
+    );
+  }, [edgeName, setEdges]);
 
   const onConnect = useCallback(
     (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
+
+  const onNodeClick = (
+    e: React.MouseEvent<Element, MouseEvent>,
+    node: Node
+  ) => {
+    setEdgeName('');
+    setId(node.id);
+    setNodeName(node.data.label);
+  };
+
+  const onEdgeClick = (e: React.MouseEvent, edge: any) => {
+    setNodeName('');
+    setId(edge.id);
+    edge.label ? setEdgeName(edge.label) : setEdgeName('');
+  };
 
   return (
     <div className={styles.wrap}>
@@ -35,10 +93,25 @@ const Flow = () => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onNodeClick={onNodeClick}
+        onEdgeClick={onEdgeClick}
+        defaultViewport={defaultViewport}
       >
         <Controls />
         <MiniMap />
         <Background gap={12} size={1} />
+        <div className={styles.updatenode__controls}>
+          <label>Node Label:</label>
+          <input
+            value={nodeName}
+            onChange={(evt) => setNodeName(evt.target.value)}
+          />
+          <label>Edge Label:</label>
+          <input
+            value={edgeName}
+            onChange={(evt) => setEdgeName(evt.target.value)}
+          />
+        </div>
       </ReactFlow>
     </div>
   );
