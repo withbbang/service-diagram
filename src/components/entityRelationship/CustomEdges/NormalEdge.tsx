6424fa43bdb2e30acddc29cff6f1520f164db524
김영선 <withbbang@gmail.com>
2023-06-16 01:05:29 +0900

    베지어 방정식을 이용하여 테이블 관계 좌표 지정 완료

--- src/components/entityRelationship/CustomEdges/NormalEdge.tsx
+++ src/components/entityRelationship/CustomEdges/NormalEdge.tsx
@@ -18,7 +18,7 @@ function NormalEdge({
   targetPosition,
   data
 }: typeCustomEdge): JSX.Element {
-  const [edgePath, middleX, middleY, offsetX, offsetY] = getBezierPath({
+  const [edgePath, middleX] = getBezierPath({
     sourceX,
     sourceY,
     sourcePosition,
@@ -26,17 +26,18 @@ function NormalEdge({
     targetY,
     targetPosition
   });
-  console.log('edgePath: ', edgePath);
-  console.log('middleX: ', middleX);
-  console.log('middleY: ', middleY);
-  console.log('offsetX: ', offsetX);
-  console.log('offsetY: ', offsetY);
-  console.log('sourceX: ', sourceX);
-  console.log('sourceY: ', sourceY);
-  console.log('targetX: ', targetX);
-  console.log('targetY: ', targetY);
 
-  const handleGetMiddle = (pos1: number, pos2: number) => (pos1 + pos2) / 2;
+  // 제어점 4개의 베지어 곡선 방정식을 이용한 좌표 반환 함수
+  // B(t) = (1 - t)³ * P0 + 3(1 - t)² * t * P1 + 3(1 - t) * t² * P2 + t³ * P3
+  const handleGetCoordinate = (posArr: Array<number>, position: string) => {
+    const t = position === 'right' ? 0.15 : 0.85;
+    return (
+      Math.pow(1 - t, 3) * posArr[0] +
+      3 * Math.pow(1 - t, 2) * t * posArr[1] +
+      3 * (1 - t) * Math.pow(t, 2) * posArr[2] +
+      Math.pow(t, 3) * posArr[3]
+    );
+  };
 
   return (
     <>
@@ -45,10 +46,13 @@ function NormalEdge({
         <div
           className={styles.label}
           style={{
-            transform: `translate(${handleGetMiddle(
-              sourceX,
-              middleX
-            )}px,${handleGetMiddle(sourceY, middleY)}px)`
+            transform: `translate(${handleGetCoordinate(
+              [sourceX, middleX, middleX, targetX],
+              sourcePosition
+            )}px, ${handleGetCoordinate(
+              [sourceY, sourceY, targetY, targetY],
+              sourcePosition
+            )}px)`
           }}
         >
           {'1'}
@@ -56,10 +60,13 @@ function NormalEdge({
         <div
           className={styles.label}
           style={{
-            transform: `translate(${handleGetMiddle(
-              targetX,
-              middleX
-            )}px,${handleGetMiddle(targetY, middleY)}px)`
+            transform: `translate(${handleGetCoordinate(
+              [sourceX, middleX, middleX, targetX],
+              targetPosition
+            )}px, ${handleGetCoordinate(
+              [sourceY, sourceY, targetY, targetY],
+              targetPosition
+            )}px)`
           }}
         >
           {'*'}
