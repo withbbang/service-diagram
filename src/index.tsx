729d531370aa32acde03d33682d76203852e886f
김영선 <withbbang@gmail.com>
2023-06-24 21:21:27 +0900

    index tsx에 쓸데없는 props 제거

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(rootNode!).render(
   <Provider store={store}>
     {/* loading: 로딩 과정을 보여줄 컴포넌트 */}
     {/* persistor: 로컬 스토리지에 저장할 store (결국 여기선 persistStore가 반환한 값) */}
-    <PersistGate loading={null} persistor={persistStore(store)}>
+    <PersistGate persistor={persistStore(store)}>
       <App />
     </PersistGate>
   </Provider>
