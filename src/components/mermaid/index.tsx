f39bdb3770cae49af654c8d4d1e63def346a26f1
김영선 <withbbang@gmail.com>
2023-08-22 21:26:42 +0900

    Fix: 저장된 mermaid 다이어그램 호출시 파싱 에러, 런타임 작성 에러 fix

--- src/components/mermaid/index.tsx
+++ src/components/mermaid/index.tsx
@@ -2,20 +2,34 @@ import { useEffect } from 'react';
 import mermaid from 'mermaid';
 import styles from './Mermaid.module.scss';
 
-mermaid.initialize({
+const initConfig = {
   startOnLoad: true,
   theme: 'default',
   securityLevel: 'loose',
   fontFamily: 'monospace'
-});
+};
 
 const Mermaid = ({ content }: typeMermaid) => {
+  const pre = document.querySelector('.mermaid');
+
   // mermaid 컨텐츠 로드
   useEffect(() => {
+    mermaid.initialize(initConfig);
     mermaid.contentLoaded();
   }, []);
 
-  return <div className={['mermaid', styles.mermaid].join(' ')}>{content}</div>;
+  useEffect(() => {
+    (async () => {
+      try {
+        const { svg } = await mermaid.render('diagram', content);
+        if (pre) {
+          pre.innerHTML = svg;
+        }
+      } catch (e) {}
+    })();
+  }, [content]);
+
+  return <pre className={['mermaid', styles.mermaid].join(' ')}>{content}</pre>;
 };
 
 interface typeMermaid {
